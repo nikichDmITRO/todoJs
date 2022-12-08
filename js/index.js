2ad413e05f2f2ace6be2e1e6c0a7056742fc5f4f
nikichDmIT <dmitracenkonikita5@gmail.com>
2022-12-08 17:20:36 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const input = document.querySelector(".input-toDo");
+const button = document.querySelector(".buttonAdd");
+
+let data = document.querySelector(".data");
+let sta
+if (localStorage.getItem(data)) {
+  sta = JSON.parse(localStorage.getItem(data));
+}
+else{
+    sta=[]
+}
+
+console.log(sta);
+data.innerHTML = sta
+  .map(
+    (el, ind) =>
+      `<div class="data-item ">${
+        ind + 1
+      }<p>${el}</p><button class="deleteBtn" data-itemid=${ind}>Удалить заметку</button></div>`
+  )
+  .join("");
+
+button.addEventListener("click", () => {
+  
+  sta.push(input.value);
+  localStorage.setItem(data, JSON.stringify(sta));
+  
+  console.log(sta);
+  data.innerHTML = sta
+    .map(
+      (el, ind) =>
+        `<div class="data-item ">${
+          ind + 1
+        }<p>${el}</p><button class="deleteBtn" data-itemid=${ind}>Удалить заметку</button></div>`
+    )
+    .join("");
+});
+
+data.addEventListener("click", (event) => {
+  if (event.target.classList.contains("deleteBtn")) {
+    
+    console.log(event.target);
+   
+    sta.splice(event.target.dataset.itemid, 1);
+    localStorage.setItem(data, JSON.stringify(sta));
+    location. reload()
+  }
+});
